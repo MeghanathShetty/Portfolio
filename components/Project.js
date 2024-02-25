6a59c071e613901c735aa317515e8320ea4d7205
Meghanath Shetty <meghanshetty3122@gmail.com>
2024-02-25 16:53:31 +0530

    Update Project.js

--- components/Project.js
+++ components/Project.js
@@ -27,7 +27,7 @@ const Project=()=>
                     </div>
                 </div>
                 <div className="project-pic mt-2">
-                    <img src="/imgs/project1_m2.png"
+                    <img src="/imgs/project1_m1.png"
                       alt="project-img" />
                 </div>
             </div>
