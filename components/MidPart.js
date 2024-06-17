cb0bf44a0e42f1565a9ace021fcff283abd44117
Meghanath Shetty <meghanshetty3122@gmail.com>
2024-06-17 17:43:59 +0530

    new color scheme ( gradient )

--- components/MidPart.js
+++ components/MidPart.js
@@ -1,6 +1,5 @@
 import { useState } from "react";
 
-
 const MidPart=()=>
 {
     // toggle skill visibilty
--- components/utils/themeStyle.js
+++ components/utils/themeStyle.js
@@ -8,14 +8,16 @@ export const darkThemeStyle = () =>
 
     .main
     {
-        background-color: black;
-        color: white;
+        // background-color: black;
+        // color: white;
+        background: linear-gradient(90deg, #000000, #390da0);
     }
 
     .top-main 
     {
         border-bottom: 2px solid #390da0;
-        background-color: black;
+        // background-color: black;
+        background: linear-gradient(90deg, #000000, #390da0);
         box-shadow: 0px 7px 30px rgba(57, 13, 160, 1);
     }
 
@@ -27,15 +29,19 @@ export const darkThemeStyle = () =>
         // box-shadow: 0px 5px 30px rgba(57, 13, 160, 1);
     }
 
-    .nav-link,.navbar-toggler
-    {
-        color:white !important;
+    .view-project a{
+        color: #00bfff;
     }
 
-    .skills .skill-btn
-    {
-        color : white !important;
-    }
+    // .nav-link,.navbar-toggler
+    // {
+    //     color:white !important;
+    // }
+
+    // .skills .skill-btn
+    // {
+    //     color : white !important;
+    // }
 
     `;
 
